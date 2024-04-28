63065078a7deb028e19f44f9694a54d0ba6e7178
FruitPassion <gamehotdl@gmail.com>
2024-04-28 10:55:47 +0200

    Fix: Add autoincrement to id column

--- app/model/shared_model.py
+++ app/model/shared_model.py
@@ -9,7 +9,7 @@ DB_SCHEMA = "main"
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
